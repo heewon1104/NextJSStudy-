332aac095c35050c26d51f51f078efc39a6201e3
김희원 <julian3306@naver.com>
2024-12-02 12:57:46 +0900

    Chapter2: NextJS 경로 개념 익히기

--- 01-getting-started/app/layout.js
+++ 01-getting-started/app/layout.js
@@ -1,10 +1,15 @@
-import './globals.css'
+import './globals.css';
 
+// 레이아웃이란? 각 페이지의 뼈대를 잡아주는 역할
+// page와 마찬가지로 각 디렉토리마다 하나의 피일이 존재해야 함
+
+// NextJS에서는 Header를 metadata라는 특별한 객체를 사용하여 페이지 이름과 설명을 설정 가능함
 export const metadata = {
   title: 'NextJS Course App',
   description: 'Your first NextJS app!',
 };
 
+// children은 현재 활성화 된 page.js를 가리킴
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
--- 01-getting-started/app/page.js
+++ 01-getting-started/app/page.js
@@ -1,9 +1,19 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
       <h1>Welcome to this NextJS Course!</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
+
+      <p>
+        {/* 다른 페이지로 이동하기 때문에 단일 페이지 내에서 머물 수 있도록 하는 Link 컴포넌트 사용 필요 */}
+        {/* <a href="/about">About US</a> */}
+        {/* 페이지를 벗어나지 않음, 페이지를 새로고침 하지 않고 다음 페이지로 이동할 수 있도록 함*/}
+        {/* 다음 페이지의 내용이 서버에 랜더링 되고, 클라이언트로 보내져 클라이언트에서 처리 후 화면에 보여줌 */}
+        <Link href="/about">About US</Link>
+      </p>
     </main>
   );
 }
